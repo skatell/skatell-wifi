021c1c1ec4e9e7d990278ee5f916f811f8e52241
skatell <skatell5540@gmail.com>
2026-08-24 09:03:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -205,6 +205,7 @@ app.post('/api/admin/refill-isp', requireAuthAPI, async (req, res) => {
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'public', 'login.html')));
 app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'public', 'admin.html')));
+app.get('/admin-login', (req, res) => res.sendFile(path.join(__dirname, 'public', 'admin.html'))); // Added to support your button link!
 app.get('/user', (req, res) => res.sendFile(path.join(__dirname, 'public', 'user.html')));
 
 // User Count Endpoint
